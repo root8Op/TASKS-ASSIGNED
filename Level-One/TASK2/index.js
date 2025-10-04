6d7803e9a806c9d472633871739c657e8c429661
root8Op <130396783+root8Op@users.noreply.github.com>
2025-10-04 20:24:34 +0530

    added comment

--- Level-One/TASK2/index.js
+++ Level-One/TASK2/index.js
@@ -6,28 +6,26 @@ document.getElementById("phone").addEventListener("input", function (e) {
 
 // end
 
-    document.getElementById("registrationForm").addEventListener("submit", function(event) {
-      event.preventDefault(); // prevents form refresh
+// Data saving functionality using Object
 
-      // Create object with input values
-      let userData = {
-        name: document.getElementById("name").value,
-        email: document.getElementById("email").value,
-        phone: document.getElementById("phone").value,
-        password: document.getElementById("password").value,
-        confirmedPassword: document.getElementById("confirmpassword").value,
-      };
+document.getElementById("registrationForm").addEventListener("submit", function(event) {
+  event.preventDefault(); // prevents form refres
+  // Create object with input values
+  let userData = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    phone: document.getElementById("phone").value,
+    password: document.getElementById("password").value,
+    confirmedPassword: document.getElementById("confirmpassword").value,
+  }
+  console.log(userData); // Print object to consol
+  let existingData = JSON.parse(localStorage.getItem("users")) || []
+  // Push new user data
+  existingData.push(userData)
+  // Save back to localStorage
+  localStorage.setItem("users", JSON.stringify(existingData))
+  console.log("Saved to localStorage:", userData);
+  alert("Data saved successfully!");
+});
 
-      console.log(userData); // Print object to console
-
-      let existingData = JSON.parse(localStorage.getItem("users")) || [];
-
-      // Push new user data
-      existingData.push(userData);
-
-      // Save back to localStorage
-      localStorage.setItem("users", JSON.stringify(existingData));
-
-      console.log("Saved to localStorage:", userData);
-      alert("Data saved successfully!");
-    });
+// end of the function code
